e142598817756aaaea05354e78920b79cba7a751
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-06-27 19:51:37 +0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -6,6 +6,13 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: function(){
+      return import(/* webpackChunkName: "about" */ '../views/auth/LoginView.vue')
+    }
   },
   {
     path: '/about',
@@ -20,7 +27,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>Arunika Online Test</h2>
+        <div id="form">
+            <div class="form-group">
+                <label for="">Nama Pengguna / Nomor Ujian</label>
+                <input type="text">
+            </div>
+            <button>Masuk</button>
+        </div>
+    </div>
+</template>
