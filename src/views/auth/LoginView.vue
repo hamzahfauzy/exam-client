245f0f3bedba18f9348503037eb42d322be4ad6e
fadillahrizki <fadillahrizki16@gmail.com>
2022-06-29 14:32:43 +0700

    handle login

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -5,11 +5,13 @@
             <h2>Arunika Test Platform</h2>
         </template>
         <template v-slot:body>
-            <div class="form-group">
-                <label for="">Nama Pengguna / Nomor Ujian</label>
-                <input type="text">
-            </div>
-            <button class="primary">Masuk</button>
+            <form @submit="handleLogin" class="form">
+                <div class="form-group">
+                    <label for="">Nama Pengguna / Nomor Ujian</label>
+                    <input type="text" v-model="username">
+                </div>
+                <button class="primary">Masuk</button>
+            </form>
         </template>
         <template v-slot:footer>
             <p>Arunika IT Department 2022</p>
@@ -22,6 +24,23 @@ import CardVue from '../../components/CardVue.vue'
 export default{
     components: { 
         CardVue,
+    },
+    data : () => ({
+        username:'',
+        API_URL:process.env.VUE_APP_API_URL
+    }),
+    methods:{
+        async handleLogin(e){
+            e.preventDefault()
+            let data = new FormData()
+            data.append('username',this.username)
+            const res = await fetch(this.API_URL +'index.php?module=auth/login',{
+                method:'POST',
+                body:data
+            })
+            const json = await res.json()
+            console.log(json)
+        },
     }
 }
 </script>
